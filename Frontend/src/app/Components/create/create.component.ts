8c63c5b74ef777d324411178b9579c76c3b40c70
silindaM <silindanemdu@gmail.com>
2021-08-29 21:46:48 +0200

    create tutorial class

---
+++ Frontend/src/app/Components/create/create.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from 'src/app/Model/tutorial';
+import { TutorialService } from 'src/app/Services/tutorial.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  tutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private tutorialService:TutorialService) { }
+
+  ngOnInit(): void {
+  }
+
+  saveTutorial(): void {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.createTutorial(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false
+    };
+  }
+
+}
