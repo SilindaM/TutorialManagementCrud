ae22d30029372fa516bb30202de40c9a0e7dd652
silindaM <silindanemdu@gmail.com>
2021-08-29 21:47:19 +0200

    project details class and template

---
+++ Frontend/src/app/Components/details/details.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tutorial } from 'src/app/Model/tutorial';
+import { TutorialService } from 'src/app/Services/tutorial.service';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit {
+  currentTutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  message = '';
+
+  constructor(
+    private tutorialService: TutorialService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.params.id);
+  }
+
+  getTutorial(id: string): void {
+    this.tutorialService.getById(id)
+      .subscribe(
+        data => {
+          this.currentTutorial = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePublished(status: boolean): void {
+    const data = {
+      title: this.currentTutorial.title,
+      description: this.currentTutorial.description,
+      published: status
+    };
+
+    this.message = '';
+
+    this.tutorialService.updateTutorial(this.currentTutorial.id, data)
+      .subscribe(
+        response => {
+          this.currentTutorial.published = status;
+          console.log(response);
+          this.message = response.message ? response.message : 'This tutorial was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateTutorial(): void {
+    this.message = '';
+
+    this.tutorialService.updateTutorial(this.currentTutorial.id, this.currentTutorial)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = response.message ? response.message : 'This tutorial was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  deleteTutorial(): void {
+    this.tutorialService.deleteById(this.currentTutorial.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/tutorials']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+}
